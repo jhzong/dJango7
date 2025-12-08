3b32ef5f72fe356c2ac5533d16ff303744f85b13
jhzong <jhzong05@gmail.com>
2025-12-08 17:23:20 +0900

    1208-1722-학생성적포털

--- d1208/stu02/student/urls.py
+++ d1208/stu02/student/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name='student'
 urlpatterns = [
     path('write/', views.write, name='write'),
+    path('list/', views.list, name='list'),
+    path('view/', views.view, name='view'),
 ]
--- d1208/stu02/student/views.py
+++ d1208/stu02/student/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+# 학생등록
 def write(request):
     return render(request,'write.html')
+# 학생리스트
+def list(request):
+    return render(request,'list.html')
+# 학생상세페이지
+def view(request):
+    return render(request,'view.html')
