cbd8cb759c420709c3c7a3a63ed6f5997179ace1
jhzong <jhzong05@gmail.com>
2025-12-08 18:17:30 +0900

    1208-1816-stu01링크걸기

--- d1208/stupjt01/student/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from . import views
-
-app_name='student'# 닉네임/별칭 정해주기
-urlpatterns = [
-    # url을 write 페이지로 넘겨줌, write 함수 호출
-    path('write/', views.write, name='write'),
-]
--- d1208/stupjt01/stupjt01/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # url을 stendent로 넘겨줌
-    path('student/', include('student.urls')),
-]
