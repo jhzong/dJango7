53889c3cbacdff4895cdef56ba66e58822b61ede
jhzong <jhzong05@gmail.com>
2025-12-08 16:42:56 +0900

    1208-1642-stupjt,jardinPjt

--- d1208/stupjt03/student03/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Student03Config(AppConfig):
-    name = 'student03'
--- d1208/stupjt03/student03/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name='student03'
-urlpatterns = [
-    path('write/', views.write, name='write'),
-]
