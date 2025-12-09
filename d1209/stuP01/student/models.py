230bc3b2d63638c322202fbd89ecf7d792ef3057
jhzong <jhzong05@gmail.com>
2025-12-09 20:20:54 +0900

    1209-2019-stuP01(pjt생성,db연결,data입력)

---
+++ d1209/stuP01/student/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# 테이블 생성-sql구문 python명령어로 대체
+class Student(models.Model):
+    id=models.CharField(max_length=100,primary_key=True)
+    pw=models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
+    age=models.IntegerField(default=0)
+    phone=models.CharField(max_length=13)
+    gender=models.CharField(max_length=10)
+    hobby=models.CharField(max_length=100)
+
+# 객체출력 - 주소값,__str__객체를 문자열로 출력
+    def __str__(self):
+        return f"{self.id},{self.name},{self.age},{self.gender}"
