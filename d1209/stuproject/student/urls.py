4a8129bb17807b3e119ba6cdde8716151a63eb0b
jhzong <jhzong05@gmail.com>
2025-12-09 17:49:30 +0900

    1209-1749

--- d1209/stuproject/student/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name='student'
-urlpatterns = [
-    # write 페이지로 연결
-    path('write/', views.write, name='write'),
-]
--- d1209/stuproject/student/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def write(request):
-    # return HttpResponse('Hello') 직접 html 출력
-    return render(request,'write.html')
