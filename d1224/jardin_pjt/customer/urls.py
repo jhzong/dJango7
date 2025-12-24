cb3714e80718d0906cfbd2a4f2aedce736dbf308
jhzong <jhzong05@gmail.com>
2025-12-24 11:53:16 +0900

    1224-1153-검색

--- d1224/jardin_pjt/customer/urls.py
+++ d1224/jardin_pjt/customer/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name='customer'
 urlpatterns = [
     path('clist/',views.clist,name='clist'),
+    path('cview/<int:bno>/',views.cview,name='cview'),
 ]
--- d1224/jardin_pjt/customer/views.py
+++ d1224/jardin_pjt/customer/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from customer.models import Board
 from django.core.paginator import Paginator
+from django.db.models import Q,F
+
+# 상세페이지
+def cview(request, bno):
+    qs=Board.objects.get()
+    return render(request,'customer/cview.html')
 
 def clist(request):
     # 검색부분--------------------------------------------
     category=request.GET.get('category','')
     search=request.GET.get('search','')
+    print('넘어온 데이터 => ',category,search)
     # //검색부분------------------------------------------
+    
     if not search:
         qs=Board.objects.all().order_by('-bgroup','bstep')
-    else:    
-        qs=Board.objects.filter(category=category,search=search)
+    else:
+        if category == 'btitle':
+            qs=Board.objects.filter(btitle__contains=search)
+        elif category == 'bcontent':
+            qs=Board.objects.filter(bcontent__contains=search)
+        elif category == 'all':
+            qs=Board.objects.filter(Q(btitle__contains=search)|Q(bcontent__contains=search))
+            
     
     # 하단 페이지넘버링-------------------------------------
     # paginator는 요청페이지 번호가 있어야함.
@@ -20,5 +34,5 @@ def clist(request):
     list_qs=paginator.get_page(page)
     # //하단 페이지넘버링-----------------------------------
     
-    context={'list':list_qs,'page':page}
+    context={'list':list_qs,'page':page,'category':category,'search':search}
     return render(request,'customer/clist.html',context)
